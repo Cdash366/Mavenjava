c99776ada118945d37d81941c396d397b05089e1
Chandan <Chandandash366@gmail.com>
2020-07-22 17:19:21 +0530

    Added Mavenjava project

---
+++ src/test/java/Demomaven/AppiumTest.java
@@ -0,0 +1,19 @@
+package Demomaven;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+
+
+	@Test
+	public void AndroidAPPs()
+	{
+		System.out.println("Android app");
+	}
+	@Test
+	public void IOSapp()
+	{
+		System.out.println("Ios app");
+	}
+	
+}
---
+++ src/test/java/Demomaven/RestAPITest.java
@@ -0,0 +1,18 @@
+package Demomaven;
+
+import org.testng.annotations.Test;
+
+public class RestAPITest {
+
+
+	@Test
+	public void PostJira()
+	{
+		System.out.println("Post jira");
+	}
+	@Test
+	public void deleteTwitter()
+	{
+		System.out.println("Delete twitter");
+	}
+}
